d4b528c72221913d59fc7622167e11d9fce96ea1
Fernando <f.sanchezvilela@mrmilu.com>
2021-04-18 16:51:07 +0200

    feat: add posts comments ssr

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,14 +1,38 @@
-const PostsContainer = ({query}) => {
-    return (
-        <h1>
-            (Post Page): you are looking the post:{query.id}
-        </h1>
-    )
-}
+//import { withRouter } from 'next/router';
+//export default withRouter(PostsContainer);
+import axios from 'axios';
 
-PostsContainer.getInitialProps =   async ({query}) => {
-    console.log(query)
-    return {query}
-}
+const Comment = ({ email, body }) => {
+  return (
+    <>
+      <h5>{email}</h5>
+      <p>{body}</p>
+    </>
+  );
+};
+
+const PostsContainer = ({ comments, query }) => {
+  return (
+    <div>
+      <h1>(Post Page): you are looking the post:{query.id}</h1>;
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id}>
+            <Comment {...comment} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+PostsContainer.getInitialProps = async ({ query }) => {
+  console.log(query);
+  const res = await axios.get(
+    'https://jsonplaceholder.typicode.com/comments?postId=3'
+  );
+  const { data } = res;
+  return { query, comments: data };
+};
 
 export default PostsContainer;
